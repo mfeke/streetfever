acf6761836c478c7ac678434a45a82dc54bec131
mfeke <mfekethohulane@gail.com>
2025-05-17 09:45:27 +0200

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,15 +5,17 @@ import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { CartComponent } from './components/cart/cart.component';
+import { ProductListsComponent } from './components/product-lists/product-lists.component';
 
 const routes: Routes = [
-    { path: '',   redirectTo: '/home', pathMatch: 'full' }, // redirect to `first-component`
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // redirect to `first-component`
 
-  { path: "account/login", component: LoginComponent},
-  { path: "account/register", component: RegisterComponent},
-  {path:"account", component:ProfileComponent},
-  { path:"home", component: HomeComponent},
-  {path:"cart", component:CartComponent}
+  { path: "account/login", component: LoginComponent },
+  { path: "account/register", component: RegisterComponent },
+  { path: "account", component: ProfileComponent },
+  { path: "home", component: HomeComponent },
+  { path: "products", component: ProductListsComponent },
+  { path: "cart", component: CartComponent }
 ];
 
 
