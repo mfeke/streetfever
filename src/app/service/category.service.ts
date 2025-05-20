21c4d23fa6f00acf494e701b426ba4345f41b9a0
mfeke <mfekethohulane@gail.com>
2025-05-20 22:30:59 +0200

    update

--- src/app/service/category.service.ts
+++ src/app/service/category.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class CategoryService {
-  private apiUrl = 'http://localhost:3000/api/category';
+  private apiUrl = 'https://streetfeverapi.vercel.app/api/category/';
 
   constructor(private http: HttpClient,) { }
 
