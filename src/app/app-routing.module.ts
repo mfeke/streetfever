3201aced9b5205a922d52e34a68be7d162bbdf61
mfeke <mfekethohulane@gail.com>
2025-06-14 20:47:09 +0200

    update navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
-import { HomeComponent } from './components/home/home.component';
-import { ProfileComponent } from './components/profile/profile.component';
-import { CartComponent } from './components/cart/cart.component';
-import { ProductListsComponent } from './components/product-lists/product-lists.component';
-import { ProductComponent } from './components/product/product.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' }, // redirect to `first-component`
+  // { path: '', redirectTo: '/home', pathMatch: 'full' }, // redirect to `first-component`
 
-  { path: "account/login", component: LoginComponent },
-  { path: "account/register", component: RegisterComponent },
-  { path: "account", component: ProfileComponent },
-  { path: "pd/:id", component: ProductComponent},
-  { path: "home", component: HomeComponent },
-  { path: "c/:name", component: ProductListsComponent },
-  { path: "cart", component: CartComponent }
+
 ];
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,44 +2,18 @@ import { NgModule } from '@angular/core';
 import { FormsModule,ReactiveFormsModule } from '@angular/forms'; 
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
-import {MatTabsModule} from '@angular/material/tabs';
-import {MatSelectModule} from '@angular/material/select';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import {MatCardModule} from '@angular/material/card';
 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
-import { HomeComponent } from './components/home/home.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { CartComponent } from './components/cart/cart.component';
-import { ProfileComponent } from './components/profile/profile.component';
-import { ProductListsComponent } from './components/product-lists/product-lists.component';
-import { OrderComponent } from './components/order/order.component';
-import { MyfilterPipe } from './myfilter.pipe';
-import { ProductComponent } from './components/product/product.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent,
-    HomeComponent,
     NavbarComponent,
-    CartComponent,
-    ProfileComponent,
-    ProductListsComponent,
-    OrderComponent,
-    MyfilterPipe,
-    ProductComponent,
 
   ],
   imports: [
@@ -47,14 +21,6 @@ import { ProductComponent } from './components/product/product.component';
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
-    MatInputModule,
-    MatButtonModule,
-    MatFormFieldModule,
-    MatIconModule,
-    MatTabsModule,
-    MatSelectModule,
-    MatProgressSpinnerModule,
-    MatCardModule,
     AppRoutingModule
   ],
   providers: [
