03b0da71caefb5fef1b0e8974db6a07bef5f887c
mfeke <mfekethohulane@gail.com>
2025-06-15 08:00:27 +0200

    update navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,11 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  bgurl = "https://i.postimg.cc/hjSW3dRD/New-wave-racket-club.jpg"
+  bgurl = "https://i.postimg.cc/nc7MbMVs/335436589-1525615111294543-225423551462420608-n.jpg"
   bgUrls = [
     { image: "https://i.postimg.cc/KcgrXMcy/women-s-denim-J-Crew.jpg", name: "denim" },
-    { image: "https://i.postimg.cc/90Snf5RF/original.jpg", name: "coats" }
+    { image: "https://i.postimg.cc/90Snf5RF/original.jpg", name: "coats" },
+    { image: "https://i.postimg.cc/jSpWnN84/HOME-d.jpg",name:"Decor" }
   ]
   name = "latest fashions"
 }
