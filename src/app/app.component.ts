9694c519a206236a680f50371167305e5682dc97
mfeke <mfekethohulane@gail.com>
2025-06-23 12:40:41 +0200

    update navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,25 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-    currentIndex = 1;
+  currentIndex = 1;
 
-  bgurl = "https://i.postimg.cc/1zMNgGHd/Grungy-Hood-Top.jpg"
+  bgurl = 'https://i.postimg.cc/6Qk65cVs/accessories.webp';
   bgUrls = [
-    { image: "https://i.postimg.cc/KcgrXMcy/women-s-denim-J-Crew.jpg", name: "denim" },
-    { image: "https://i.postimg.cc/90Snf5RF/original.jpg", name: "coats" },
-    { image: "https://i.postimg.cc/jSpWnN84/HOME-d.jpg",name:"Decor" }
-  ]
-  urls={
-    img:["https://i.postimg.cc/j2RfC8VB/Ladies-Leather-Purse-Coffee-color.jpg",
-      "https://i.postimg.cc/jSSGyKQS/dunhill.jpg",
-      "https://i.postimg.cc/vTvN6S4n/dress.jpg",
-      "https://i.postimg.cc/26rdBjhH/download-23.jpg"
+    { image: 'https://i.postimg.cc/90Snf5RF/original.jpg', name: 'coats' },
+    {
+      image: 'https://i.postimg.cc/KcgrXMcy/women-s-denim-J-Crew.jpg',
+      name: 'denim',
+    },
+    { image: 'https://i.postimg.cc/mrDBSqjW/Sage-introductions-to-the-Represent-mainline.jpg', name: 'accessories' },
+  ];
+  urls = {
+    img: [
+      'https://i.postimg.cc/j2RfC8VB/Ladies-Leather-Purse-Coffee-color.jpg',
+      'https://i.postimg.cc/jSSGyKQS/dunhill.jpg',
+      'https://i.postimg.cc/vTvN6S4n/dress.jpg',
+      'https://i.postimg.cc/26rdBjhH/download-23.jpg',
     ],
-    name:"THE GIFT GUIDE"
-  }
-  name = "latest fashions"
-
+    name: 'THE GIFT GUIDE',
+  };
+  name = 'WINTER ARRIVALS';
 }
